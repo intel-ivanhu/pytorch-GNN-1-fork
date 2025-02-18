9d38d683c965d0ec5fe2cd2bce033168ce6b815c
Ivan Hu <108011383+intel-ivanhu@users.noreply.github.com>
2025-02-18 13:46:45 +0800

    Commit to Main
    Commit to Main

--- code_01_moons.py
+++ code_01_moons.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
-"""
-@author: 代码医生工作室
-@公众号：xiangyuejiqiren   （内有更多优秀文章及学习资料）
-@来源: <PyTorch深度学习和图神经网络（卷 1）——基础知识>配套代码 
-@配套代码技术支持：bbs.aianaconda.com  
-Created on Fri Feb  1 00:07:25 2019
-"""
 
 import sklearn.datasets     #引入数据集
 import torch
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from code_02_moons_fun import LogicNet,plot_losses,predict,plot_decision_boundary
 
@@ -27,23 +21,33 @@ plt.show()
 
 
 
-model = LogicNet(inputdim=2,hiddendim=3,outputdim=2)#初始化模型
+model = LogicNet()#初始化模型
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)#定义优化器
 
 
 xt = torch.from_numpy(X).type(torch.FloatTensor)#将Numpy数据转化为张量
 yt = torch.from_numpy(Y).type(torch.LongTensor)
+
 epochs = 1000#定义迭代次数
 losses = []#定义列表，用于接收每一步的损失值
+lr_list = []
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer,step_size=50,gamma=0.99)
 for i in range(epochs):
     loss = model.getloss(xt,yt)
     losses.append(loss.item())
     optimizer.zero_grad()#清空之前的梯度
     loss.backward()#反向传播损失值
     optimizer.step()#更新参数
+    scheduler.step()
+    #if i %50 == 0:
+    #    for p in optimizer.param_groups:
+    #        p['lr'] *= 0.99
+    lr_list.append(optimizer.state_dict()['param_groups'][0]['lr'])
+plt.plot(range(epochs),lr_list,color = 'r')
+plt.show()
 
 
-
+print(losses)
 plot_losses(losses)
 
 
